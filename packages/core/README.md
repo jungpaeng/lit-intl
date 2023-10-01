ba819c0c38c6bde57a727a18cac95f5b303d05f6
jungpaeng <meis1541@naver.com>
2023-10-01 20:36:24 +0900

    feat: Add test code

--- packages/core/README.md
+++ packages/core/README.md
@@ -1,16 +1,16 @@
-# lit-intl
+# @lit-intl/core
 
-[![Stable release](https://img.shields.io/npm/v/lit-intl.svg)](https://npm.im/lit-intl)
+[![Stable release](https://img.shields.io/npm/v/@lit-intl/core.svg)](https://npm.im/@lit-intl/core)
 
 This is minimal i18n for react package.
 
 ```jsx
-function Follower({user}) {
+function Follower({ user }) {
   const t = useTranslation('Follower');
 
   return (
     <>
-      <Text>{t('latestFollower', {username: user.name})}</Text>
+      <Text>{t('latestFollower', { username: user.name })}</Text>
       <IconButton aria-label={t('followBack')} icon={<FollowIcon />} />
     </>
   );
@@ -36,17 +36,90 @@ function Follower({user}) {
 
 1. Install `@lit-intl/core` in your project
 2. Add the provider your top code
+
 ```jsx
-import {LitIntlProvider} from '@lit-intl/core';
+import { LitIntlProvider } from '@lit-intl/core';
 
 export default function App() {
   return (
     <LitIntlProvider messages={messages}>
-      <Component {...pageProps} />
+      <Component />
     </LitIntlProvider>
   );
 }
 ```
+
 3. Based on the features you need, you might have to provide [polyfills](https://formatjs.io/docs/polyfills).
 4. Use translations in your components!
 
+## Usage
+
+```js
+// message
+{
+  Basic: {
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#basic-principles
+    static: 'Hello',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#simple-argument
+    simpleArgument: 'Hello {name}',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#number-type
+    currency: '{price, number, ::currency/EUR}',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#number-type
+    percent: 'Almost {pctBlack, number, ::percent} of them are black.',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#date-type
+    date: 'Sale begins {start, date, medium}',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#time-type
+    time: 'Coupon expires at {expires, time, short}',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#select-format
+    select: '{ gender, select, male {He} female {She} other {They} } will respond shortly.',
+
+    nestedSelect:
+      '{ taxableArea, select, yes {An additional {taxRate, number, percent} tax will be collected.} other {No taxes apply.} }',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#plural-format
+    plural: 'Cart: {itemCount} {itemCount, plural, one {item} other {items} }',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#selectordinal-format
+    selectordinal:
+      "It's my cat's {year, selectordinal, one {#st} two {#nd} few {#rd} other {#th}} birthday!",
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#rich-text-formatting
+    // https://formatjs.io/docs/intl-messageformat/#rich-text-support
+    richText:
+      'Our price is <boldThis>{price, number, ::currency/USD precision-integer}</boldThis> with <link>{pct, number, ::percent} discount</link>',
+  },
+}
+```
+
+```tsx
+function Component() {
+  const t = useTranslation('Basic');
+
+  return (
+    <p>{t('static')}</p>
+    <p>{t('simpleArgument', { name: 'world' })}</p>
+    <p>{t('currency', { price: 400 })}</p>
+    <p>{t('percent', { pctBlack: 0.4 })}</p>
+    <p>{t('date', { start: new Date('2023-10-01') })}</p>
+    <p>{t('time', { expires: new Date('2023-10-01') })}</p>
+    <p>{t('select', { gender: 'male' })}</p>
+    <p>{t('nestedSelect', { taxableArea: 'yes', taxRate: 0.1 })}</p>
+    <p>{t('plural', { itemCount: 1 })}</p>
+    <p>{t('selectordinal', { year: 1 })}</p>
+    <p>
+      {t('richText', {
+        boldThis: (children: React.ReactNode) => <b>{children}</b>,
+        link: (children: React.ReactNode) => <i>{children}</i>,
+        price: 10000,
+        pct: 0.1,
+      })}
+  </p>
+  );
+}
+```
--- packages/core/test/use-translation.test.tsx
+++ packages/core/test/use-translation.test.tsx
@@ -8,10 +8,41 @@ import { useTranslation } from '../src/use-translation';
 
 const message: IntlMessage = {
   Basic: {
-    Hello: 'Hello',
-    'Hello {name}': 'Hello {name}',
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#basic-principles
+    static: 'Hello',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#simple-argument
+    simpleArgument: 'Hello {name}',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#number-type
+    currency: '{price, number, ::currency/EUR}',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#number-type
+    percent: 'Almost {pctBlack, number, ::percent} of them are black.',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#date-type
+    date: 'Sale begins {start, date, medium}',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#time-type
+    time: 'Coupon expires at {expires, time, short}',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#select-format
+    select: '{ gender, select, male {He} female {She} other {They} } will respond shortly.',
+
+    nestedSelect:
+      '{ taxableArea, select, yes {An additional {taxRate, number, percent} tax will be collected.} other {No taxes apply.} }',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#plural-format
+    plural: 'Cart: {itemCount} {itemCount, plural, one {item} other {items} }',
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#selectordinal-format
+    selectordinal:
+      "It's my cat's {year, selectordinal, one {#st} two {#nd} few {#rd} other {#th}} birthday!",
+
+    // https://formatjs.io/docs/core-concepts/icu-syntax/#rich-text-formatting
+    // https://formatjs.io/docs/intl-messageformat/#rich-text-support
     richText:
-      'This is <important>important</important> and <important><very>important</very></important>',
+      'Our price is <boldThis>{price, number, ::currency/USD precision-integer}</boldThis> with <link>{pct, number, ::percent} discount</link>',
   },
 };
 
@@ -33,20 +64,62 @@ function renderMessage(message: string, values?: TranslationValue) {
 }
 
 it('should be print Hello', () => {
-  renderMessage('Hello');
+  renderMessage('static');
   screen.getByText('Hello');
 });
 
 it('should be print Hello with name value', () => {
-  renderMessage('Hello {name}', { name: 'world' });
+  renderMessage('simpleArgument', { name: 'world' });
   screen.getByText('Hello world');
+});
+
+it('should be print number currency format', () => {
+  renderMessage('currency', { price: 400 });
+  screen.getByText('€400.00');
+});
+
+it('should be print number percent format', () => {
+  renderMessage('percent', { pctBlack: 0.4 });
+  screen.getByText('Almost 40% of them are black.');
+});
+
+it('should be print date format', () => {
+  renderMessage('date', { start: new Date('2023-10-01') });
+  screen.getByText('Sale begins Oct 1, 2023');
+});
+
+it('should be print time format', () => {
+  renderMessage('time', { expires: new Date('2023-10-01') });
+  screen.getByText('Coupon expires at 9:00 AM');
+});
+
+it('should be print select format', () => {
+  renderMessage('select', { gender: 'male' });
+  screen.getByText('He will respond shortly.');
+});
+
+it('should be print nestedSelect format', () => {
+  renderMessage('nestedSelect', { taxableArea: 'yes', taxRate: 0.1 });
+  screen.getByText('An additional 10% tax will be collected.');
+});
+
+it('should be print plural format', () => {
+  renderMessage('plural', { itemCount: 1 });
+  screen.getByText('Cart: 1 item');
+});
+
+it('should be print selectordinal format', () => {
+  renderMessage('selectordinal', { year: 1 });
+  screen.getByText("It's my cat's 1st birthday!");
 });
 
 it('should be print rich text', () => {
   const { container } = renderMessage('richText', {
-    important: (children: React.ReactNode) => <b>{children}</b>,
-    very: (children: React.ReactNode) => <i>{children}</i>,
+    boldThis: (children: React.ReactNode) => <b>{children}</b>,
+    link: (children: React.ReactNode) => <i>{children}</i>,
+    price: 10000,
+    pct: 0.1,
   });
 
-  expect(container.innerHTML).toBe('This is <b>important</b> and <b><i>important</i></b>');
+  expect(container.innerHTML).toBe('Our price is <b>$10,000</b> with <i>10% discount</i>');
 });
